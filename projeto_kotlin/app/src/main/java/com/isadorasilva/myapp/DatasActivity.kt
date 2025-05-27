c23f48cb3eac3cc5c10cbfb37b427b606fd02c6b
Isadora Silva <isadoracristyne3@gmail.com>
2025-05-27 18:25:30 -0300

    Create DatasActivity.kt

---
+++ projeto_kotlin/app/src/main/java/com/isadorasilva/myapp/DatasActivity.kt
@@ -0,0 +1,7 @@
+package com.isadorasilva.quartetoelegante
+
+class DatasActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_datas)
+    }
