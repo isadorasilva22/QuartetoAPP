988f78e2fa7f0d0aca3177a971cbefb1b012a685
Isadora Silva <isadoracristyne3@gmail.com>
2025-05-30 14:01:58 -0300

    Update and rename projeto_kotlin/app/src/main/java/com/isadorasilva/myapp/DatasActivity.kt to projeto_kotlin/app/src/main/java/com/isadorasilva/myapp/quartetoelegante/DatasActivity.kt

--- projeto_kotlin/app/src/main/java/com/isadorasilva/myapp/DatasActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.isadorasilva.quartetoelegante
-
-class DatasActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_datas)
-    }
