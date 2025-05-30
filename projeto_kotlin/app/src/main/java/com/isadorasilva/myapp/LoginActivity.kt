1a775f43385b8b9517a2b04f19466e4aea7ce85e
Isadora Silva <isadoracristyne3@gmail.com>
2025-05-30 14:02:22 -0300

    Update LoginActivity.kt

--- projeto_kotlin/app/src/main/java/com/isadorasilva/myapp/LoginActivity.kt
+++ projeto_kotlin/app/src/main/java/com/isadorasilva/myapp/LoginActivity.kt
@@ -1,21 +1,11 @@
-package com.isadorasilva.quartetoelegante
+package com.isadorasilva.myapp.quartetoelegante
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        val loginButton = findViewById<Button>(R.id.login_button)
-        loginButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish() //
-        }
     }
 }
-
